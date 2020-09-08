7c08eedf74bd16f85de7592105e0a3d990c9d1ab
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-08 19:26:28 +0300

    Initial Commit

---
+++ For.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+int main()
+{
+    int i, N;
+    setlocale(LC_ALL, "Rus");
+    printf("Введите число N: ");  
+    scanf("%d", &N);      
+    for (i = 1; i <= N; i++)
+    {
+        printf("Квадрат числа %d равен %d\n", i, i * i);
+    }
+    getchar();
+}
