55dffbc81546857d7d97d043f03baf8b00998f04
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-08 23:36:24 +0300

    For- Commit №2

--- For.cpp
+++ For.cpp
@@ -4,13 +4,16 @@
 
 int main()
 {
-    int i, N;
-    setlocale(LC_ALL, "Rus");
-    printf("Введите число N: ");  
-    scanf("%d", &N);      
-    for (i = 1; i <= N; i++)
+    float S = 0, z = 1, c = 1, d = 2;
+    int i;
+    setlocale(LC_ALL, "Rus");     
+    for (i = 1; i <= 20; i++)
     {
-        printf("Квадрат числа %d равен %d\n", i, i * i);
+        S = S + z * c / d;
+        z = -z;
+        c++;
+        d = 2 * d;
     }
+    printf("Сумма S = %f", S);
     getchar();
 }
